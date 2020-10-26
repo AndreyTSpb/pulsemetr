3c5226bc9531d40b736cfc9d9c66f7109c4ebc8e
Enot Potaskun <potaskun@debian.debian.local>
2020-10-26 13:34:40 +0300

    Оптимизация и продакшен версия

---
+++ dist/js/script.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    /**
+     * 
+     */
+    $('.carusel__slider').slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      //adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left-arrow.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            dots: true,
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+    /**
+     * 
+     */
+    $('input[type="phone"]').mask("+7(999) 999-9999");
+
+    /**
+     * 
+     */
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+          });
+      });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    /**
+     *  Modal 
+     */ 
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    /**
+     * Валидация форм
+     */
+    function validateForm(form){
+      $(form).validate({
+        rules: {
+          // simple rule, converted to {required:true}
+          name: "required",
+          // compound rule
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        }
+      });
+    }
+
+    validateForm('#get_consultation form');
+    validateForm('#consultation form');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass');
+const cleanCSS     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const rename       = require("gulp-rename");
+const htmlmin      = require('gulp-htmlmin');
+const imagemin     = require('gulp-imagemin');
 
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -17,7 +19,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/scss/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
@@ -25,9 +27,58 @@ gulp.task('styles', function() {
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
-
+/**
+ * Watch to change
+ */
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('minify'));
+    gulp.watch("src/css/*").on('change', gulp.parallel('css'));
 })
+/**
+ *  Compreaaed html pages
+ */
+gulp.task('minify', ()=>{
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist/'));
+});
+/**
+ *  Transfer js to dist
+ */
+gulp.task('script', ()=>{
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.dest('dist/js'));
+});
+/**
+ *  Transfer css to dist
+ */
+gulp.task('css', ()=>{
+    return gulp.src('src/css/**/*')
+        .pipe(gulp.dest('dist/css'));
+});
+/**
+ *  Transfer font to dist
+ */
+gulp.task('font', ()=>{
+    return gulp.src('src/font/**/*')
+        .pipe(gulp.dest('dist/font'));
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+/**
+ * compressed images
+ */
+gulp.task("imageminimize", ()=>{
+    return gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+});
+/**
+ * Compressed icons
+ */
+gulp.task("iconsmize", ()=>{
+    return gulp.src('src/icons/**/*')
+        .pipe(gulp.dest('dist/icons'))
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'iconsmize', 'imageminimize', 'script', 'font', 'minify', 'css'));
